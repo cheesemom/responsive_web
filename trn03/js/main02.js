45ce0926d39cb08aa02e69a731f50c40dda7b4ba
cheesemom <smsm2610@naver.com>
2022-08-08 17:22:31 +0900

    0808

--- trn03/js/main02.js
+++ trn03/js/main02.js
@@ -1,8 +1,47 @@
 $(function () {
-    $('.main_slider').slick({
+
+    var closeBanner = () => {
+        $('.TopBanner').toggleClass('on');
+        //$('.TopBanner').slideToggle();
+        //document.querySelector('.TopBanner').classList.toggle('on')
+    }
+    $('.TopBanner i').on('click', closeBanner);
+
+    $('.MainSlider').slick({
         arrows: false,
         dots: true,
         autoplay: true,
         autoplaySpeed: 2000,
     });
+
+    $('.ProductSlider').slick({
+        arrows: false,
+        slidesToShow: 5,
+        responsive: [
+            {
+                breakpoint: 1230,
+                settings: {
+                    slidesToShow: 1,
+                    dots: true
+                }
+            }
+        ]
+    });
+
+
+    function toggleClass() {
+        $('.pop li').toggleClass('on');
+        $('.Footer .popup').toggleClass('on');
+    }
+
+    $('.pop li').on('click', toggleClass);
+    $('.popup i').on('click', toggleClass);
+
+    function mopen() {
+        $(this).toggleClass('on');
+        $('.Gnb').toggleClass('on');
+    }
+
+    $('.mopen').on('click', mopen)
+
 })
